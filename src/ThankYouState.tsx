ddd75cbf301153971719e2caeb4f633a8139acd0
AIY7788 <aiykhounnakhon@gmail.com>
2026-02-20 10:25:59 +0700

    fIx: bg color

--- src/ThankYouState.tsx
+++ src/ThankYouState.tsx
@@ -8,12 +8,12 @@ type Props = {
 function ThankYouState({ rating, setShowThankYouState }: Props) {
   return (
     <div
-      className="max-w-86 fixed mx-5 flex flex-col items-center gap-6 bg-gray-900 px-5 py-7 rounded-xl sm:px-7 sm:py-10 sm:max-w-90 sm:rounded-3xl"
+      className="max-w-86 fixed mx-5 flex flex-col items-center gap-6 bg-grey-900 px-5 py-7 rounded-xl sm:px-7 sm:py-10 sm:max-w-90 sm:rounded-3xl"
       onClick={() => setShowThankYouState(false)}
     >
       <IllustrationThankYou width={162} height={108} />
 
-      <p className="text-primary bg-grey-900 py-2 px-4 rounded-4xl w-fit">
+      <p className="text-primary bg-grey-800 py-2 px-4 rounded-4xl w-fit">
         You selected <span>{rating}</span> out of 5
       </p>
 
