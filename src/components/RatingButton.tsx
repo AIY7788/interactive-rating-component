64b1dd35bd96197b829fa054a2f04ccb62cfb159
AIY7788 <aiykhounnakhon@gmail.com>
2026-02-20 10:27:49 +0700

    fix: bg-color and condition onclick

--- src/components/RatingButton.tsx
+++ src/components/RatingButton.tsx
@@ -1,17 +1,20 @@
 type Props = {
   value: number;
   rating: number | null;
+  showThankYouState: boolean;
   setRating: React.Dispatch<React.SetStateAction<number | null>>;
 };
 
-function RatingButton({ value, rating, setRating }: Props) {
+function RatingButton(
+  { value, rating, showThankYouState, setRating }: Props
+) {
   const isActive = rating === value;
 
   return (
     <li>
       <button
-        className={`w-11 h-11 flex items-center justify-center rounded-full transition-colors duration-300 ${isActive ? "bg-white text-grey-950" : "bg-grey-900 hover:bg-primary hover:text-grey-950"} cursor-pointer`}
-        onClick={() => setRating(value)}
+        className={`w-11 h-11 flex items-center justify-center rounded-full transition-colors duration-300 ${isActive ? "bg-white text-grey-950" : "bg-grey-800 hover:bg-primary hover:text-grey-950"} cursor-pointer`}
+        onClick={() => !showThankYouState && setRating(value)}
       >
         {value}
       </button>
