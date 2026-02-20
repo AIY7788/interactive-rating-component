ae436a2292f5352e33949678215da521a64b3075
AIY7788 <aiykhounnakhon@gmail.com>
2026-02-20 10:22:59 +0700

    Add auto hide for rating validation message tooltip

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ThankYouState from "./ThankYouState";
 import RatingState from "./RatingState";
 
@@ -6,9 +6,19 @@ function App() {
   const [rating, setRating] = useState<number | null>(null);
   const [showThankYouState, setShowThankYouState] = useState(false);
 
+  useEffect(() => {
+    if (!rating && showThankYouState) {
+      const timeoutID = setTimeout(() => {
+        setShowThankYouState(false);
+      }, 1500);
+
+      return () => clearTimeout(timeoutID);
+    }
+  }, [rating, showThankYouState]);
+
   return (
     <div className="min-h-screen relative bg-grey-950 font-overpass font-regular text-body text-grey-500 flex items-center justify-center p-5">
-      {showThankYouState ? (
+      {showThankYouState && rating ? (
         <ThankYouState
           rating={rating}
           setShowThankYouState={setShowThankYouState}
@@ -18,6 +28,7 @@ function App() {
           rating={rating}
           setRating={setRating}
           setShowThankYouState={setShowThankYouState}
+          showThankYouState={showThankYouState}
         />
       )}
     </div>
